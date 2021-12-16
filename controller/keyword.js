4b9ff29e46e214b3f1bf84db08b634a23b90b9f5
Mihir Shah <shahmihir507@gmail.com>
2021-12-16 21:27:47 +0530

    Added keyword insert & Sort

--- controller/keyword.js
+++ controller/keyword.js
@@ -1,3 +1,4 @@
+const { listenerCount } = require('../models/video');
 const Video = require('../models/video')
 exports.testing = async(req,res,next) => {
     try{
@@ -24,6 +25,12 @@ exports.getVideo = async(req,res,next) => {
                 success: true,
                 message: "Not Found"
             }))
+        }
+        var qList = v.query;
+        for(var i=0;i<qList.length;i++){
+            var k = qList[i].key;
+            var c = qList[i].count;
+            console.log(k+":"+c);
         }
         res.status(200).json({
             success: true,
@@ -62,4 +69,84 @@ exports.insertVideo = async (req,res,next) => {
             error: err
         })
     }
+}
+
+exports.insertKey = async (req,res,next) => {
+    try{
+        const videoId = req.params.Id;
+        const {key} = req.body;
+        console.log(key);
+        const video = await Video.findOne({videoId:videoId});
+        if(!video){
+            const videoNew = await Video.create({videoId:videoId,query: [{key: key,count:1}]});
+            if(!videoNew){
+                return next(res.status(400).json({
+                    success: true,
+                    message: "Some error occured please try again later"
+                }));
+            }
+            return res.status(200).json({
+                success: true,
+                message:"Successfully inserted",
+                video: videoNew
+            });
+        }
+        var qList = video.query;
+        var flag = false;
+        var ind = -1;
+        for(var i=0;i<qList.length;i++){
+            var k = qList[i].key;
+            var c = qList[i].count;
+            if(k == key){
+                qList[i].count = c+1;
+                ind = i;
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            qList.push({key: key,count: 1});
+            ind = qList.length - 1;
+        }
+        
+        qList = checkAndSort(qList,ind);
+        
+        const videoUpdated = await Video.findOneAndUpdate(videoId,{query:qList},{
+            new: true,
+            newValidators: true
+        });
+        return res.status(200).json({
+            success: true,
+            message:"Successfully key inserted/updated",
+            video: videoUpdated});
+    }catch(err){
+        console.log(err.stack);
+        res.status(400)
+        .json({
+            success: false,
+            error: err
+        })
+    }
+}
+
+function checkAndSort(qList, keyIndex){
+    var keyCurr = qList[keyIndex].key;
+    var countCurr = qList[keyIndex].count;
+    if(keyIndex-1 >= 0){
+        var k = keyIndex-1;
+        while(k>=0 && countCurr > qList[k].count){
+            k=k-1;
+        }
+        if(k<0){
+            qList.splice(keyIndex,1);
+            qList.splice(0,0,{"key":keyCurr,"count":countCurr});
+            console.log(qList[0])
+        }else if(!(countCurr > qList[k].count)){
+            qList.splice(keyIndex,1);
+            qList.splice(k+1,0,{"key":keyCurr,"count":countCurr});
+            console.log(qList[k+1]);
+        }
+    }
+    
+    return qList;
 }
--- routes/keyword.js
+++ routes/keyword.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {testing,getVideo,insertVideo} = require('../controller/keyword')
+const {testing,getVideo,insertVideo,insertKey} = require('../controller/keyword')
 router.route('/:Id').get(getVideo).post(insertVideo);
+router.route('/key/:Id').post(insertKey);
 module.exports = router
